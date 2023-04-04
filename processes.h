368a6a8e2c8583e8bbc78c0d12ef93c1bc738965
SkinnyMarco <105551233+MarcoLucas41@users.noreply.github.com>
2023-04-04 19:25:18 +0100

    Update processes.h

--- processes.h
+++ processes.h
@@ -16,40 +16,41 @@
 #include <pthread.h>
 
 
-
 #ifndef PROJETOSO_PROCESSES_H
 #define PROJETOSO_PROCESSES_H
+
+
+#define MAX_SIZE 256
 
 #define SENSOR_PIPE "sensor_pipe"
 #define CONSOLE_PIPE "console_pipe"
 
+int config[5];
 
 
+typedef struct SensorData
+{
+    char *key;
+    int latest_value;
+    int min_value;
+    int max_value;
+    double avg_values;
+    int counter_updates_key;
+    struct SensorData *next;
+}SensorData;
 
 
+typedef struct ArrayData
+{
+    SensorData *first;
+    SensorData *last;
+}ArrayData;
 
+typedef struct SharedMemory
+{
+    ArrayData *header;
+    char **sensors;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}SharedMemory;
 
 #endif //PROJETOSO_PROCESSES_H
