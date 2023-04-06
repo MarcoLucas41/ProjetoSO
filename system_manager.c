7e7798a50c94a037c6e9090e56a5020e870ffa7b
broilasx <93016182+broilasx@users.noreply.github.com>
2023-04-06 02:48:12 +0100

    Add files via upload

--- system_manager.c
+++ system_manager.c
@@ -2,12 +2,16 @@
 // Created by Marco Lucas on 31/03/2023.
 //
 #include "processes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
 
 //setup
 int* SETUP();
 
 //threads
-void *DISPATCHER(void *id);
 void *SENSOR_READER(void *id);
 void *CONSOLE_READER(void *id);
 
@@ -15,7 +19,64 @@ void *CONSOLE_READER(void *id);
 void WORKER(int id);
 void ALERTS_WATCHER(int id);
 
+#define QUEUE_SZ 10
+#define MAX_LEN_MSG 100
 
+typedef struct {
+    char messages[QUEUE_SZ][MAX_LEN_MSG];
+    int ri; //index do read
+    int wi; //index do write
+    int count;
+    pthread_mutex_t lock;
+    pthread_cond_t not_full;
+    pthread_cond_t not_empty;
+} MessageQueue;
+
+void MessageQueueInitial(MessageQueue *queue)
+{
+    memset(queue->messages, 0, sizeof(queue->messages));
+    queue->ri = 0; //index do read
+    queue->wi = 0; //index do write
+    queue->count = 0;
+
+    pthread_mutex_init(&queue->lock, NULL);
+    pthread_cond_init(&queue->not_full, NULL);
+    pthread_cond_init(&queue->not_empty, NULL);
+}
+
+void MessageQueueEnqueue(MessageQueue *queue, const char *message)
+{
+    pthread_mutex_lock(&queue->lock);
+    while (queue->count == QUEUE_SZ) {
+        pthread_cond_wait(&queue->not_full, &queue->lock);
+    }
+
+    strncpy(queue->messages[queue->wi], message, MAX_LEN_MSG);
+    queue->wi = (queue->wi + 1) % QUEUE_SZ;
+    queue->count++;
+
+    pthread_mutex_unlock(&queue->lock);
+    pthread_cond_signal(&queue->not_empty);
+}
+
+void MessageQueueDequeue(MessageQueue *queue, char *message)
+{
+    pthread_mutex_lock(&queue->lock);
+
+    while (queue->count == 0) {
+        pthread_cond_wait(&queue->not_empty, &queue->lock);
+    }
+
+    strncpy(message, queue->messages[queue->ri], MAX_LEN_MSG);
+    queue->messages[queue->ri][0] = '\0';
+
+    // Clearing the message
+    queue->ri = (queue->ri + 1) % QUEUE_SZ;
+    queue->count--;
+
+    pthread_mutex_unlock(&queue->lock);
+    pthread_cond_signal(&queue->not_full);
+}
 
 int* SETUP()
 {
@@ -30,12 +91,6 @@ int* SETUP()
     }
     return configurations;
 }
-
-void *DISPATCHER(void *id)
-{
-
-}
-
 
 void *CONSOLE_READER(void *id)
 {
@@ -89,7 +144,40 @@ void ALERTS_WATCHER(int id)
 
 }
 
+//READER THREAD
+void *READER_THREAD_FUNCTION(void *id)
+{
+    MessageQueue *queue = (MessageQueue *)id;
+    char msg[MAX_LEN_MSG];
 
+    while (1) {
+        // Reading from sensor pipe or console input
+        // ...
+
+        // Enqueueing message
+        MessageQueueEnqueue(queue, msg);
+    }
+
+    return NULL;
+}
+
+//DISPATCHER THREAD
+void *DISPATCHER_THREAD_FUNCTION(void *id)
+{
+    MessageQueue *queue = (MessageQueue *)id;
+    char msg[MAX_LEN_MSG];
+
+    while (1) {
+        // Dequeueing message
+        MessageQueueDequeue(queue, msg);
+
+        // Processing message
+        // ...
+
+        printf("The received message is: %s\n", msg);
+    }
+    return NULL;
+}
 
 
 int main(int argc, char *argv[])
@@ -104,7 +192,8 @@ int main(int argc, char *argv[])
     //funcoes de destroir named pipes
     //unlink(SENSOR_PIPE);
     //unlink(SENSOR_PIPE);
-
+    MessageQueue queue;
+    MessageQueueInitial(&queue);
 
     //setting up configurations
     int *config = SETUP();
@@ -143,6 +232,13 @@ int main(int argc, char *argv[])
 
     thread_id[2] = 2;
     pthread_create(&threads[0],NULL,CONSOLE_READER,&thread_id[2]);
+
+    pthread_t reader_thread, dispatcher_thread;
+    pthread_create(&reader_thread, NULL, READER_THREAD_FUNCTION, &queue);
+    pthread_create(&dispatcher_thread, NULL, DISPATCHER_THREAD_FUNCTION, &queue);
+
+    pthread_join(reader_thread, NULL);
+    pthread_join(dispatcher_thread, NULL);
 
     //signal(SIGINT,funcao de limpeza do sistema);
 
