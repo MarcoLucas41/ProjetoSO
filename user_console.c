803eb67fe929003bda39c56643ed19215101d645
SkinnyMarco <105551233+MarcoLucas41@users.noreply.github.com>
2023-05-06 14:40:32 +0100

    Update user_console.c

--- user_console.c
+++ user_console.c
@@ -1,80 +1,36 @@
 //
-// Created by Marco Lucas on 31/03/2023.
+// Marco Lucas 2021219146
+// Bruno Almeida 2021237081
+
+// Sistemas Operativos
+// Licenciatura em Engenharia Informática 2022/2023
 //
 #include "processes.h"
-void menu(int fd);
 
-char *buffer;
-char *temp;
-char *ptr;
+void menu();
+
+char buffer[MAX_LEN_MSG];
 int fd;
 
 //user console SIGINT
 void ctrlc_handler(int signal_num)
 {
-
     printf("Received SIGINT signal (Ctrl+C). Cleaning up resources and exiting...\n");
     // Cleanup resources here...
-    free(ptr);
-    free(temp);
-    free(buffer);
     close(fd);
-    // ...
-
     exit(0);
 }
 
 
-void menu(int fd)
+void menu()
 {
-    buffer = (char*)malloc(MAX_LEN_MSG * sizeof(char));
     while(1)
     {
-        temp = (char*)malloc(MAX_LEN_MSG * sizeof(char));
-        fgets(temp, MAX_LEN_MSG, stdin);
-        strcpy(buffer,temp);
-
-        if ((strlen(temp) > 0) && (temp[strlen(temp) - 1] == '\n') )
-            temp[strlen(temp) - 1] = '\0';
-
-
-        char *delim = " ";
-        ptr = strtok(temp, delim);
-
-        if(strcmp(ptr,"exit") == 0)
-        {
-            write(fd,buffer,sizeof(buffer));
-            free(ptr);
-            free(temp);
-            free(buffer);
-            close(fd);
-            printf("Console turning off...\n");
-            break;
-        }
-        else if( strcmp(buffer,"add_alert") == 0 )
-        {
-
-        }
-        else if(strcmp(buffer,"remove_alert") == 0)
-        {
-
-        }
-        else if( strcmp(ptr,"list_alerts") == 0 || strcmp(ptr,"stats") == 0 ||
-                 strcmp(ptr,"reset") == 0 || strcmp(ptr,"sensors") == 0)
-        {
-            write(fd,buffer,sizeof(buffer));
-        }
-
-        else
-        {
-            free(temp);
-            free(ptr);
-            printf("Command not found\n");
-            continue;
-        }
+        fgets(buffer, MAX_LEN_MSG, stdin);
+        if ((strlen(buffer) > 0) && (buffer[strlen(buffer) - 1] == '\n'))
+            buffer[strlen(buffer) - 1] = '\0';
         write(fd,buffer,sizeof(buffer));
     }
-    exit(0);
 }
 
 
@@ -82,19 +38,20 @@ void menu(int fd)
 
 int main(int argc, char *argv[])
 {
+    signal(SIGINT,ctrlc_handler);
     if(argc != 2)
     {
-        perror("Wrong number of arguments for user console!");
-        exit(0);
+        printf("Wrong number of arguments for user console.\n");
+        exit(1);
     }
-    int fd;
-    if ((fd = open(CONSOLE_PIPE, O_WRONLY)) < 0)
+    printf("Starting CONSOLE_READER FOR WRITING\n");
+    if ( ( fd = open(CONSOLE_PIPE, O_WRONLY) ) < 0)
     {
-        perror("Cannot open pipe for writing: ");
+        perror("Cannot open CONSOLE PIPE for WRITING: ");
         exit(0);
     }
-    signal(SIGINT,ctrlc_handler);
-    menu(fd);
+    printf("after OPENING CONSOLE PIPE FOR WRITING\n");
+    menu();
     exit(0);
 
 }
